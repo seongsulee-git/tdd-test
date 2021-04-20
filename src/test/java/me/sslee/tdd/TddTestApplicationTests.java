37ff0969a18b4099137249625d416ad3d648303f
SeongsuLee <dev.leeseongsu@gmail.com>
2021-04-20 18:23:37 +0900

    init project

---
+++ src/test/java/me/sslee/tdd/TddTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sslee.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
